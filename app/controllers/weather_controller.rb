8989d5a7f2d273bd94324be5d468a5228fa1a4ec
jason <jduong89@gmail.com>
2022-03-30 22:02:50 -0700

    adds weather index action/route

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,2 +1,6 @@
 class WeatherController < ApplicationController
+  def index
+    response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?lat=#{params[:lat]}&lon=#{params[:lon]}&appid=#{Rails.application.credentials.openweathermap.api_key}")
+    render json: response.parse(:json)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/facts" => "facts#index"
   get "/myip" => "myip#index"
+  get "/weather" => "weather#index"
 end
